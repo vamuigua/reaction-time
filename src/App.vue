b95dd54b8c75ff395ee2b503bc5c90a64d03c3f1
Victor Allen <vamuigua@gmail.com>
2021-01-23 16:15:11 +0300

    update: Added Timer to calculate reaction time
    Timer uses setInterval at 10ms to keep track of how long the player has taken before clicking the block
    Added a Results component that displays the score and rank
    Added styling to components

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,35 @@
 <template>
   <h1>Reaction Time</h1>
   <button @click="start" :disabled="isPlaying">Play</button>
-  <Block v-if="isPlaying" :delay="delay" />
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <Results v-if="showResults" :score="score" />
 </template>
 
 <script>
 import Block from "./components/Block.vue";
+import Results from "./components/Results.vue";
 
 export default {
   name: "App",
-  components: { Block },
+  components: { Block, Results },
   data() {
     return {
       isPlaying: false,
       delay: null,
+      score: null,
+      showResults: false,
     };
   },
   methods: {
     start() {
       this.delay = 2000 + Math.random() * 5000;
       this.isPlaying = true;
+      this.showResults = false;
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResults = true;
     },
   },
 };
@@ -33,5 +43,20 @@ export default {
   text-align: center;
   color: #444;
   margin-top: 60px;
+}
+button {
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled] {
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="block" v-if="showBlock">Click Me</div>
+  <div class="block" v-if="showBlock" @click="stopTimer">Click Me</div>
 </template>
 
 <script>
@@ -8,12 +8,26 @@ export default {
   data() {
     return {
       showBlock: false,
+      timer: null,
+      reactionTime: 0,
     };
   },
   mounted() {
     setTimeout(() => {
       this.showBlock = true;
+      this.startTimer();
     }, this.delay);
+  },
+  methods: {
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.reactionTime += 10;
+      }, 10);
+    },
+    stopTimer() {
+      clearInterval(this.timer);
+      this.$emit("end", this.reactionTime);
+    },
   },
 };
 </script>
@@ -27,5 +41,6 @@ export default {
   text-align: center;
   padding: 100px 0;
   margin: 40px auto;
+  cursor: pointer;
 }
 </style>
