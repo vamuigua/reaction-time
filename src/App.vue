b2208db94c9c1bfd351b1c88b892b7cf918f8a00
Victor Allen <vamuigua@gmail.com>
2021-02-19 20:47:38 +0300

    feat: Added PWA functionaity to the app The app's service worker is created and registered by the workbox library The app receives updates whenever the serviceworker is updated with new data

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,28 @@
   <p v-if="isPlaying">Get Ready...</p>
   <Block v-if="isPlaying" :delay="delay" @end="endGame" />
   <Results v-if="showResults" :score="score" />
+
+  <!-- Update Dialog Prompt -->
+  <div class="update-dialog" v-if="prompt">
+    <div class="update-dialog__content">
+      A new version is found. Refresh to load it?
+    </div>
+    <div class="update-dialog__actions">
+      <button
+        class="update-dialog__button update-dialog__button--confirm"
+        @click="update"
+      >
+        Update
+      </button>
+      <button
+        class="update-dialog__button update-dialog__button--cancel"
+        @click="prompt = false"
+      >
+        Cancel
+      </button>
+    </div>
+  </div>
+  <!-- End of Update Dialog Prompt -->
 </template>
 
 <script>
@@ -34,9 +56,23 @@ export default {
       delay: null,
       score: null,
       showResults: false,
+      prompt: false,
     };
   },
+  created() {
+    if (this.workbox) {
+      // Notify the user if a service worker has installed but is stuck waiting to activate
+      this.workbox.addEventListener("waiting", () => {
+        this.prompt = true;
+      });
+    }
+  },
   methods: {
+    async update() {
+      this.prompt = false;
+      // Send a message to the waiting service worker, instructing it to activate.
+      await this.workbox.messageSW({ type: "SKIP_WAITING" });
+    },
     start() {
       this.delay = 2000 + Math.random() * 5000;
       this.isPlaying = true;
@@ -91,5 +127,32 @@ button[disabled] {
 
 .my-link a:hover {
   background-color: #54be92;
+}
+
+.update-dialog {
+  position: fixed;
+  left: 50%;
+  bottom: 64px;
+  transform: translateX(-50%);
+  border-radius: 4px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+  padding: 12px;
+  max-width: 576px;
+  color: white;
+  background-color: #2c3e50;
+  text-align: left;
+}
+
+.update-dialog__actions {
+  display: flex;
+  margin-top: 8px;
+}
+
+.update-dialog__button {
+  margin-right: 8px;
+}
+
+.update-dialog__button--confirm {
+  margin-left: auto;
 }
 </style>
---
+++ vue.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  pwa: {
+    name: "Reaction Time",
+    themeColor: "#4FB088",
+    msTileColor: "#4FB088",
+    appleMobileWebAppCache: "yes",
+    appleMobileWebAppStatusBarStyle: 'black',
+    iconPaths:{
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/msapplication-icon-144x144.png'
+    },
+    manifestOptions: {
+        background_color: "#FFFFFF",
+    }
+  }
+};
